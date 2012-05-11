75c5895632b5af6b3539e89df14e9a6a0486d261
creature <creature@1eedc636-7f7f-0410-9624-aebde32d8031>
2012-05-11 20:43:35 +0000

    shared object

---
+++ shared_object/shared_object.cpp
@@ -0,0 +1,110 @@
+#include "shared_object.h"
+
+#include <iostream>
+#include <sys/shm.h>
+#include <errno.h>
+#include <stdexcept>
+#include <pthread.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+void * shared_object_base::operator new(unsigned int type_size)
+{
+    key_t key = 12035;
+
+    int shmid = shmget(key, type_size, IPC_CREAT|0666);
+    //int shmid = shmget(key, type_size, 0666);
+
+    if (-1 == shmid )
+    {
+       throw std::runtime_error("Critical Error while creating shared mem!");
+    }
+
+    shared_object_base* new_obj = (shared_object_base*)shmat(shmid,NULL,0);
+
+    if (NULL == new_obj)
+    {
+        throw std::runtime_error("Critical Error while creating shared mem2!");
+    }
+
+    // give allocated object his shmid
+    new_obj->m_shmid = shmid;
+
+    std::cout << "created shmem at " << new_obj << std::endl;
+
+    return new_obj;
+}
+
+void shared_object_base::operator delete(void *p)
+{
+    shared_object_base* new_obj = (shared_object_base*)p;
+    shmdt(new_obj);
+    std::cout << "deleted shmem at " << new_obj << std::endl;
+}
+
+shared_object_base::shared_object_base()
+{
+
+}
+
+shared_object_base::~shared_object_base()
+{
+
+}
+
+shared_object_execute_base::shared_object_execute_base()
+    : terminate(false)
+{
+    pthread_mutexattr_t attrs;
+    pthread_mutexattr_init(&attrs);
+    pthread_mutexattr_setpshared(&attrs, PTHREAD_PROCESS_SHARED);
+
+    pthread_mutex_init(&mutex_command_pending, &attrs);
+    pthread_mutex_init(&mutex_result_pending, &attrs);
+
+    pthread_mutexattr_destroy(&attrs);
+}
+
+shared_object_execute_base::~shared_object_execute_base()
+{
+    terminate = true;
+    pthread_mutex_unlock(&mutex_command_pending);
+    usleep(10000); // wait 10ms
+    pthread_mutex_destroy(&mutex_command_pending);
+    pthread_mutex_destroy(&mutex_result_pending);
+}
+
+void shared_object_execute_base::startSlave()
+{
+    // initial state:
+    // mutex_command_pending: locked
+    // mutex_result_pending: locked
+    pthread_mutex_lock(&mutex_command_pending);
+    pthread_mutex_lock(&mutex_result_pending);
+
+    __pid_t pid = fork();
+    if (pid == 0)
+    {
+        // child: command loop
+        while (!terminate)
+        {
+            // wait for new command master
+            pthread_mutex_lock(&mutex_command_pending);
+            if (!terminate)
+            {
+                execute_command();
+            }
+            pthread_mutex_unlock(&mutex_result_pending);
+        }
+        std::cout << "slave is terminating" << std::endl;
+        exit(0);
+    }
+}
+
+void shared_object_execute_base::triggerSlave()
+{
+    pthread_mutex_unlock(&mutex_command_pending);
+    pthread_mutex_lock(&mutex_result_pending);
+}
---
+++ shared_object/shared_object.h
@@ -0,0 +1,159 @@
+#ifndef SHARED_OBJECT_H
+#define SHARED_OBJECT_H
+
+#include <stdexcept>
+#include <iostream>
+#include <sys/types.h>
+
+class shared_object_base
+{
+private:
+    int m_shmid;
+public:
+    shared_object_base();
+    virtual ~shared_object_base();
+    void * operator new(unsigned int);
+    void operator delete (void *p);
+};
+
+class shared_object_execute_base: public shared_object_base
+{
+private:
+    pthread_mutex_t mutex_command_pending;
+    pthread_mutex_t mutex_result_pending;
+    bool terminate;
+protected:
+    virtual void execute_command() = 0;
+public:
+    shared_object_execute_base();
+    ~shared_object_execute_base();
+    void startSlave();
+    void triggerSlave();
+};
+
+template<class _objT>
+class shared_func_call
+{
+public:
+    // be called in slave
+    virtual void execute_function(_objT* self) = 0;
+};
+
+template<class _objT, class _resultT, class _fpT>
+class shared_func_call0: public shared_func_call<_objT>
+{
+private:
+    _resultT (_objT::*func_p)();
+    _resultT result;
+
+public:
+    shared_func_call0(_resultT (_objT::*a_func_p)())
+        : func_p(a_func_p) {}
+
+    // be called in slave
+    void execute_function(_objT* self)
+    {
+        result = (self->*func_p)();
+    }
+
+    _resultT getResult()
+    {
+        return result;
+    }
+};
+
+template<class _objT, class _resultT, class _fpT, class _pT1>
+class shared_func_call1: public shared_func_call<_objT>
+{
+private:
+    _resultT (_objT::*func_p)(_pT1);
+    _pT1 param1;
+    _resultT result;
+
+public:
+    shared_func_call1(_resultT (_objT::*a_func_p)(_pT1), _pT1 a_param1)
+        : func_p(a_func_p), param1(a_param1) {}
+
+    // be called in slave
+    void execute_function(_objT* self)
+    {
+        result = (self->*func_p)(param1);
+    }
+
+    _resultT getResult()
+    {
+        return result;
+    }
+};
+
+template<class _parentT, bool _is_shared, size_t _param_buffer_size = 1024>
+class shared_object: private _parentT, // this inheritence is forces private cause noone should call memberfunctions directly! Use call_function_X instead!
+                     public shared_object_execute_base
+{
+private:
+    typedef shared_object<_parentT, _is_shared, _param_buffer_size> thisType;
+    char data_buffer[_param_buffer_size];
+
+    void execute_command()
+    {
+        shared_func_call<thisType>* call = (shared_func_call<thisType>*)data_buffer;
+        call->execute_function(this);
+    }
+
+public:
+    shared_object() {}
+
+    template <typename _resultT, typename _fpT>
+    _resultT call_function_0(_fpT func)
+    {
+        if (_is_shared)
+        {
+            // check size
+            if (sizeof(shared_func_call0<thisType, _resultT, _fpT>) > _param_buffer_size)
+            {
+                throw std::runtime_error("shared_object::call_function(): Sizeof function call is to big!");
+            }
+            // creates function call object
+            shared_func_call0<thisType, _resultT, _fpT>* call = new (data_buffer) shared_func_call0<thisType, _resultT, _fpT>(func);
+
+            // let slave work:
+            triggerSlave();
+
+            // wait to be executed
+            return call->getResult();
+        }
+        else
+        {
+            // not shared: simply call function
+            return (this->*func)();
+        }
+    }
+
+    template <class _resultT, class _p1T, class _fpT>
+    _resultT call_function_1(_fpT func, _p1T param1)
+    {
+        if (_is_shared)
+        {
+            // check size
+            if (sizeof(shared_func_call1<thisType, _resultT, _fpT, _p1T>) > _param_buffer_size)
+            {
+                throw std::runtime_error("shared_object::call_function(): Sizeof function call is to big!");
+            }
+            // creates function call object
+            shared_func_call1<thisType, _resultT, _fpT, _p1T>* call = new (data_buffer) shared_func_call1<thisType, _resultT, _fpT, _p1T>(func, param1);
+
+            // let slave work:
+            triggerSlave();
+
+            // wait to be executed
+            return call->getResult();
+        }
+        else
+        {
+            // not shared: simply call function
+            return (this->*func)(param1);
+        }
+    }
+};
+
+#endif // SHARED_OBJECT_H
