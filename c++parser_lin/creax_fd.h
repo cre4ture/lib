d5b665781d0bc92d90f92654a861ffb2cca9e749
creature <creature05@arcor.de>
2012-05-27 18:40:15 +0000

    improvements

--- c++parser_lin/creax_fd.h
+++ c++parser_lin/creax_fd.h
@@ -2,6 +2,7 @@
 #define CREAX_FD_H
 
 #include <fcntl.h>
+#include <stdexcept>
 
 namespace creax
 {
@@ -12,19 +13,19 @@ namespace creax
         int mfd;
 
     public:
-        fd(const std::string& filename)
+        fd(const std::string& filename, int flags = O_RDONLY)
         {
-            mfd = open(filename.c_str(), O_RDONLY);
+            mfd = open(filename.c_str(), flags, S_IRUSR | S_IWUSR); // in case of O_CREATE set file permissions: S_IRUSR | S_IWUSR
             if (mfd == -1)
                 throw std::runtime_error("creax::fd(): failed to open file: " + filename);
         }
 
-        int getFd()
+        int getFd() const
         {
             return mfd;
         }
 
-        void getStat(struct stat &statbuf)
+        void getStat(struct stat &statbuf) const
         {
             if (fstat(mfd,&statbuf) < 0)
                 throw std::runtime_error("creax::fd::getStat(): fstat error");
--- c++parser_lin/threadfifo.cpp
+++
@@ -1,1 +0,0 @@
-#include "threadfifo.h"
